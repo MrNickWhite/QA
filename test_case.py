d05499f9123e8b9fe497beb4763a4bf607347869
Nikolai P <mr.whitestrider@gmail.com>
2021-03-08 18:02:40 +0300

    First commit. Created user test for jsonplaceholder

---
+++ test_case.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def test_get_user():
+    correct_response = """{
+  "id": 2,
+  "name": "Ervin Howell",
+  "username": "Antonette",
+  "email": "Shanna@melissa.tv",
+  "address": {
+    "street": "Victor Plains",
+    "suite": "Suite 879",
+    "city": "Wisokyburgh",
+    "zipcode": "90566-7771",
+    "geo": {
+      "lat": "-43.9509",
+      "lng": "-34.4618"
+    }
+  },
+  "phone": "010-692-6593 x09125",
+  "website": "anastasia.net",
+  "company": {
+    "name": "Deckow-Crist",
+    "catchPhrase": "Proactive didactic contingency",
+    "bs": "synergize scalable supply-chains"
+  }
+}"""
+    correct_response = json.loads(correct_response)
+    response = requests.get('https://jsonplaceholder.typicode.com/users/2').json()
+    assert correct_response == response 
